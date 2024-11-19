1e49bfe2676f6e5a63031cfda9bc1ac039509903
Piotr Truszkowski <truszkowski@gmail.com>
2024-11-19 09:12:54 +0100

    with dependency

--- main.tf
+++ main.tf
@@ -8,12 +8,16 @@ terraform {
   }
 }
 
-resource random_pet suffix {
+resource random_pet pet {
   length = 1
 }
 
+output PET {
+  value = random_pet.pet.id
+}
+
 resource "spacelift_stack" "stack" {
-  name       = "sp5ft-stack-${random_pet.suffix.id}"
+  name       = "sp5ft-stack-${random_pet.pet.id}"
   branch     = "main"
   raw_git {
     url = "https://github.com/truszkowski/sp5ft-example"
@@ -22,9 +26,15 @@ resource "spacelift_stack" "stack" {
   project_root = "stack"
 }
 
-resource "spacelift_environment_variable" "" {
-  stack_id   = spacelift_stack.stack.id
-  name       = "TF_VAR_PET"
-  value      = "${random_pet.suffix.id}"
-  write_only = false
+data "spacelift_current_stack" "this" {}
+
+resource "spacelift_stack_dependency" "dep" {
+  stack_id            = spacelift_stack.stack.id
+  depends_on_stack_id = data.spacelift_current_stack.this.id
+}
+
+resource "spacelift_stack_dependency_reference" "ref" {
+  stack_dependency_id = spacelift_stack_dependency.dep.id
+  output_name         = "PET"
+  input_name          = "TF_VAR_PET"
 }
